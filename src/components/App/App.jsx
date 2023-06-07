38a0265bf4530e66dd6613a74eac0c956d6ca496
Will Richardson <rich1051@umn.edu>
2023-06-07 12:31:33 -0500

    1st lecture done, GET request made

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,40 @@
-import BookList from '../BookList/BookList';
-import BookForm from '../BookForm/BookForm';
+import BookList from "../BookList/BookList";
+import BookForm from "../BookForm/BookForm";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
 
   // TODO - GET Book List from server
+  // axios.get('/stuff')
+  // .then(when response comes back, update redux)
+
+  const fetchBookList = () => {
+    axios
+      .get("/books")
+      .then((response) => {
+        // send data to redux store with dispatch
+        dispatch({ type: 'SET_BOOK_LIST', payload: response.data });
+      })
+      .catch((error) => {
+        console.log("Error fetching book list:", error);
+      });
+  };
+
+  // {useEffect} here:
+  useEffect(() => {
+    fetchBookList();
+  }, []);
 
   return (
     <div className="App">
-      <header><h1>Books w/ Redux!</h1></header>
+      <header>
+        <h1>Books w/ Redux!</h1>
+      </header>
       <main>
         <BookForm />
         <BookList />
--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'; 
-import logger from 'redux-logger';
-import './index.css';
-import App from './components/App/App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import "./index.css";
+import App from "./components/App/App";
 
 const bookList = (state = [], action) => {
   // TODO - set book list with data from server
+  if (action.type === "SET_BOOK_LIST") {
+    // payload is response.data from GET to /books
+    // payload/response.data is all of the books
+    // this will replace old book list with fresh book list
+    // don't need spread operator: don't care about old value
+    return action.payload;
+  }
   return state;
-}
+};
 
 const reduxStore = createStore(
   combineReducers({
-    bookList
+    bookList,
   }),
   applyMiddleware(logger)
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={reduxStore}>
@@ -27,4 +33,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
